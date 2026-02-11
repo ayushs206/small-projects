447dd6f24ff69dca162325151388168abe43f19b
Ayush <ayushsingla206@gmail.com>
2026-02-11 18:36:06 +0530

    Fixed Path redirecting

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const path = require("path");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const QRCode = require("qrcode");
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.post("/generate", async (req, res) => {
     const { url } = req.body;
